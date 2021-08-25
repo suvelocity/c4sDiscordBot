132381e95cf45bdc4a751f9a5382e46465d6fb65
Shahar Eliyahu <shahar.e3@gmail.com>
2021-08-25 17:49:40 +0300

    chore(md): fix

--- index.ts
+++ index.ts
@@ -1,10 +1,24 @@
 import { Bot } from './src'
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+global.fetch = require('node-fetch')
+
 require('dotenv').config()
 
 const main = async () => {
   const bot = new Bot()
   bot.start({ token: process.env.BOT_TOKEN! })
 }
+
+const keepAwake = () => {
+  try {
+    fetch('https://discordc4sbot.herokuapp.com/')
+    console.log('keep awake')
+  } catch ({ message }) {
+    console.log(message)
+  }
+}
+
+setInterval(keepAwake, 1000 * 60 * 15)
 
 main()
